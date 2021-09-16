066bbdd402ef9acf9dee10a804763fb3677e47d5
Daniel Fagerstrom <daniel@danielfagerstrom.com>
2021-09-16 17:22:52 +0200

    Refactored CT template.

--- dwgenerator/sql/standard/create_table.sql
+++ dwgenerator/sql/standard/create_table.sql
@@ -3,14 +3,10 @@
 DROP TABLE IF EXISTS {{ target_table.full_name }};
 
 CREATE TABLE {{ target_table.full_name }} (
+  {% for column in target_table.columns %}
+  {{ "%-20s" | format(column.name) }} {{ column.type }}{{"," if not loop.last or target_table.pk }}
+  {% endfor %}
   {% if target_table.pk %}
-  {% for column in target_table.columns %}
-  {{ "%-20s" | format(column.name) }} {{ column.type }}{{","}}
-  {% endfor %}
   CONSTRAINT pk_{{ target_table.name }} PRIMARY KEY ({% for pk in target_table.pk %}{{ pk.name }}{{", " if not loop.last }}{% endfor %})
-  {% else %}
-  {% for column in target_table.columns %}
-  {{ "%-20s" | format(column.name) }} {{ column.type }}{{"," if not loop.last }}
-  {% endfor %}
   {% endif %}
 );
