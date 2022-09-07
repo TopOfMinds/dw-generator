f67ca61a43cbcd0321ad0afcd0aee6f4bc648a5c
Daniel Fagerstrom <daniel@danielfagerstrom.com>
2022-09-07 09:37:41 +0200

    Modernized the package build using pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name = "dwgenerator",
     version = "0.1",
     url = "https://github.com/TopOfMinds/dw-generator",
     description="Data Warehouse Generator",
-    entry_points='''
-        [console_scripts]
-        dwgenerator=dwgenerator.cli:cli
-    ''',
-    packages = find_packages(),
-    install_requires=[
-        'Click',
-        'Jinja2',
-        'colorama' ],
+    packages = find_namespace_packages(),
     setup_requires=['setuptools_git'],
     include_package_data=True,
 )
